3a412904955feb0b8840408540cde07c011526d4
Administrator <Administrator@DESKTOP-099UQRG>
2018-09-25 20:00:15 -0400

    Initial Commit of Spring Boot Project

---
+++ src/test/java/com/plantplaces4/plantplaces4/Plantplaces4ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.plantplaces4.plantplaces4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Plantplaces4ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
